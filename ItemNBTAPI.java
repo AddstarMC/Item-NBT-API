2537ca349faa2cc74d4e676ca1bf21fbbfd7da75
Narimm <benjicharlton@gmail.com>
2018-09-26 16:29:08 +1000

    Update nbtapi
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- ItemNBTAPI.java
+++ ItemNBTAPI.java
@@ -12,7 +12,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.tr7zw.itemnbtapi.utils.MinecraftVersion;
+import au.com.addstar.monolith.util.nbtapi.utils.MinecraftVersion;
 
 public class ItemNBTAPI extends JavaPlugin {
 
